043330e9722463fed0e0fae346e987a822d71aaa
AthenaKuo329 <kuo20170408@gmail.com>
2023-06-03 10:26:34 +0800

    modify

---
+++ lesson9_1.py
@@ -0,0 +1,10 @@
+# https://docs.streamlit.io/library/api-reference
+import streamlit as st
+
+st.write("## Hello world Robert") #streamlit 重點在顯示網頁資料，無法調整格式位置
+#" "內的文字隨時更新時，只要在網頁重新整理就會跟著變更
+# 有支援Markdown語法，如##，參考語法大全 https://hackmd.io/@eMP9zQQ0Qt6I8Uqp2Vqy6w/SyiOheL5N/%2FBVqowKshRH246Q7UDyodFA?type=book
+
+st.write("Hello ***嗨***") 
+st.write(">>#### Hello Athena") 
+st.write(">>>> # <https://google.com>") 
